e9a1a27effad8db0c386734f6e15bafc821e2203
ksugawara61 <ksugawara61@users.noreply.github.com>
2021-11-23 16:28:01 +0900

    fix: coverage

--- app/Services/CalculatePointService.php
+++ app/Services/CalculatePointService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Exceptions\PreConditionException;
+use App\Exceptions\PreconditionException;
 
 final class CalculatePointService
 {
--- tests/Unit/CalculatePointServiceTest.php
+++ tests/Unit/CalculatePointServiceTest.php
@@ -3,27 +3,41 @@
 namespace Tests\Unit;
 
 use App\Services\CalculatePointService;
+use App\Exceptions\PreconditionException;
 use PHPUnit\Framework\TestCase;
 
 class CalculatePointServiceTest extends TestCase
 {
+    public function dataProvider_for_calcPoint(): array
+    {
+        return [
+            '購入金額が0なら0ポイント' => [0, 0],
+            '購入金額が999なら0ポイント' => [0, 999],
+            '購入金額が1000なら10ポイント' => [10, 1000],
+            '購入金額が9999なら99ポイント' => [99, 9999],
+            '購入金額が10000なら200ポイント' => [200, 10000]
+        ];
+    }
+
     /**
      * @test
+     * @dataProvider dataProvider_for_calcPoint
      */
-    public function calcPoint_購入金額が0ならポイントは0()
+    public function calcPoint(int $expected, int $amount)
     {
-        $result = CalculatePointService::calcPoint(0);
+        $result = CalculatePointService::calcPoint($amount);
 
-        $this->assertSame(0, $result);
+        $this->assertSame($expected, $result);
     }
 
     /**
      * @test
      */
-    public function calcPoint_購入金額が1000ならポイントは10()
+    public function calcPoint_購入金額が負の数なら例外をスロー()
     {
-        $result = CalculatePointService::calcPoint(1000);
+        $this->expectException(PreconditionException::class);
+        $this->expectExceptionMessage('購入金額が負の数');
 
-        $this->assertSame(10, $result);
+        CalculatePointService::calcPoint(-1);
     }
 }
